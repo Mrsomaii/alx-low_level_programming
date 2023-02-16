21fb651166b28084237c80776278ceaa9397957d
Mrsomaii <ojansomai88@gmail.com>
2023-02-16 13:50:17 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#includes <stdio.h>
+/*
+ * main - print line of code using puts
+ *
+ * Return: 0
+*/
+int main(void)
+{	puts("\""Programming is like building a multilingual puzzle);
+	Return(0);
+}
